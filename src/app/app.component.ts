ebe4e72b34adf7e004dc8eafb537069760a169a0
Anna Lukianova <ann.lookyanova@gmail.com>
2023-10-05 00:16:22 +0200

    Implement stopwatch logic and add style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
 
+interface LapTime {
+  count: number;
+  time: any;
+  lapTime?: any;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'timer';
+  public isRunning: boolean = false;
+
+  private lastTime = Date.now();
+  private requestId: number = 0;
+  public timeElapsed = 0;
+  public lapTimeElapsed: number = 0;
+
+  public lapTimes: LapTime[] = [];
+  public isLapClicked: boolean = false;
+
+  onStart() {
+    this.isRunning = true;
+    this.lastTime = Date.now();
+    const onAnimationFrame = () => {
+      this.step();
+      this.requestId = requestAnimationFrame(onAnimationFrame);
+    };
+    this.requestId = requestAnimationFrame(onAnimationFrame);
+  }
+
+  onPause() {
+    cancelAnimationFrame(this.requestId);
+    this.isRunning = false;
+  }
+
+  onLap() {
+    this.lapTimes.unshift({
+      count: this.lapTimes.length + 1,
+      time: this.timeElapsed,
+      lapTime: this.lapTimeElapsed,
+    });
+
+    this.isLapClicked = true;
+    this.lapTimeElapsed = 0;
+  }
+
+  onClear() {
+    this.timeElapsed = 0;
+    this.onPause();
+    this.lapTimes = [];
+    this.isLapClicked = false;
+  }
+
+  step() {
+    const now = Date.now();
+    const dt = now - this.lastTime;
+    this.lastTime = now;
+
+    this.timeElapsed += dt;
+    this.lapTimeElapsed += dt;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DeveloperInfoComponent } from './developer-info/developer-info.component';
+import { HeadingZeroNumberPipe } from './pipes/heading-zero-number.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeveloperInfoComponent,
+    HeadingZeroNumberPipe
   ],
   imports: [
     BrowserModule
