9612ad982d123e456af33abde7b3b822504239e3
Anna Lukianova <ann.lookyanova@gmail.com>
2023-10-05 00:15:17 +0200

    Create developer info component

---
+++ src/app/developer-info/developer-info.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'developer-info',
+  templateUrl: './developer-info.component.html',
+  styleUrls: ['./developer-info.component.css'],
+})
+export class DeveloperInfoComponent {}
