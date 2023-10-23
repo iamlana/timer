bf6c14e9f776e710926552b9fac224d3a55f4b25
Anna Lukianova <ann.lookyanova@gmail.com>
2023-10-23 14:56:05 +0200

    Get rid of unnecessary pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DeveloperInfoComponent } from './developer-info/developer-info.component';
-import { HeadingZeroNumberPipe } from './pipes/heading-zero-number.pipe';
 
 @NgModule({
-  declarations: [AppComponent, DeveloperInfoComponent, HeadingZeroNumberPipe],
+  declarations: [AppComponent, DeveloperInfoComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pipes/heading-zero-number.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'headingZeroNumber' })
-export class HeadingZeroNumberPipe implements PipeTransform {
-  transform(value: number): string {
-    return value < 10 ? `0${value}` : `${value}`;
-  }
-}
